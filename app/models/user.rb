ffbbca1d658fdf64e0c99a79dfac679e5a630081
Adam Gołąb <edek-150@o2.pl>
2016-05-17 11:54:35 +0200

    followers system done

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :active_relationships, class_name: 'Follower',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: 'Follower',
+                                   foreign_key: 'user_id',
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships,
+                       source: :user
+  has_many :followers, through: :passive_relationships,
+                       source: :follower
+
+  def follow(other_user)
+    active_relationships.create(user_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(user_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517083430) do
+ActiveRecord::Schema.define(version: 20160517092451) do
+
+  create_table "followers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "follower_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "followers", ["follower_id"], name: "index_followers_on_follower_id"
+  add_index "followers", ["user_id"], name: "index_followers_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
