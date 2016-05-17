90007c4ac5c6b76d9882be4c96af1b2344ebc2ad
Adam Gołąb <edek-150@o2.pl>
2016-05-17 12:50:41 +0200

    attached image to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@
 # - user (belongs_to)
 # - replies (has_many)
 # - reply_to (belongs_to)
-# - image (has_one)
+# - image (has_attached_file managed by Paperclip)
 # - activities (belongs_to)
 class Post < ActiveRecord::Base
   belongs_to :reply_to, class_name: 'Post', foreign_key: 'reply_to_id'
@@ -13,4 +13,7 @@ class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :post_tags
   has_many :tags, through: :post_tags
+  has_attached_file :image, styles: { medium: ['1920x1080>', :jpg], thumb: ['200x200#', :jpg] }
+  # default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: %r{image\/.*}
 end
