a98e14f1c410aebb717e97820bdf5848303a20d0
Adam Gołąb <edek-150@o2.pl>
2016-05-18 16:33:53 +0200

    pagination on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,6 @@ class HomeController < ApplicationController
     @posts = Post.where(author: Post.where('author_id IN (?) OR author_id = ?',
                                            current_user.following.collect(&:id),
                                            current_user.id))
+                 .paginate(page: params[:page], per_page: 20)
   end
 end
