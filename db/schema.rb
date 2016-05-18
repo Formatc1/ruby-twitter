2abd9d38d3df75f2172a661b1e0cede014e80a65
Adam Gołąb <edek-150@o2.pl>
2016-05-18 11:09:58 +0200

    added visible name field to User with edition

--- db/schema.rb
+++ db/schema.rb
@@ -66,19 +66,19 @@ ActiveRecord::Schema.define(version: 20160518082630) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,      null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "username",               default: "test", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "username",               default: "", null: false
     t.string   "visible_name"
   end
 
