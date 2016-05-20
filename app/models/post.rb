db0fffeea3141e4952db9e56d2e23a7b0a1289e9
Adam Gołąb <edek-150@o2.pl>
2016-05-20 15:28:10 +0200

    uploading images to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Paperclip
+
   field :content, type: String
   field :created_at, type: DateTime
   belongs_to :user, inverse_of: :posts
@@ -7,6 +9,11 @@ class Post
   has_and_belongs_to_many :liked_by, class_name: 'User',
                                      inverse_of: :liked_posts
 
+  has_mongoid_attached_file :image, styles: {
+    original: ['1920x1080>', :jpg],
+    thumb: ['200x200#', :jpg]
+  }
+  validates_attachment_content_type :image, content_type: %r{image\/.*}
   validates :content, presence: true
 
   before_create do |post|
