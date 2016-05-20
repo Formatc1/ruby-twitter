2bcab9ab6a8d425b15f2f834d72e4e59dace0fb1
Adam Gołąb <edek-150@o2.pl>
2016-05-20 14:37:07 +0200

    user page with his own tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get 'home', to: 'home#index'
+  get 'user/:username', to: 'users#show_posts', as: 'user'
   post 'posts/create'
 
   root to: 'home#index'
