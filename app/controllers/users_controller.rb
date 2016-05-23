7fccf9e166b138c3600e49afefed04a03a234c1f
Adam Gołąb <edek-150@o2.pl>
2016-05-23 16:01:31 +0200

    listing posts with specific tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @posts = User.where(username: params[:username])
                  .first
                  .posts
+                 .order(created_at: :desc)
                  .paginate(page: params[:page], per_page: 20)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'home', to: 'posts#index'
   get 'user/:username', to: 'users#show_posts', as: 'user'
+  get 'tag/:tag', to: 'tags#show', as: 'tag'
   post 'posts/create'
   post 'posts/like/:id', to: 'posts#like', as: 'post_like'
   delete 'posts/unlike/:id', to: 'posts#unlike', as: 'post_unlike'
