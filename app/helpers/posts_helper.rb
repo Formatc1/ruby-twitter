6b88df3cc34b073ff196033c0baafd25911873be
Adam Gołąb <edek-150@o2.pl>
2016-05-23 16:46:49 +0200

    displaying post with links to hashtags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def render_with_hashtags(content)
+    content.gsub(/(?<tag>#\w+)/) { hashtag_link(Regexp.last_match[:tag]) }
+  end
+
+  def hashtag_link(tag)
+    link_to html_escape(tag), tag_path(tag[1..-1])
+  end
 end
