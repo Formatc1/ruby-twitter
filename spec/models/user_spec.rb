b4a8eb752e779bb6119cb5eb2310220388a7a76b
Adam Gołąb <edek-150@o2.pl>
2016-06-09 11:04:02 +0200

    added cleanup after test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'follow other user' do
-    user1 = User.create!(email: 'user1@example.com',
-                         password: 'testtest',
-                         username: 'user1',
-                         visible_name: 'user1')
-    user2 = User.create!(email: 'user2@example.com',
-                         password: 'testtest',
-                         username: 'user2',
-                         visible_name: 'user2')
-    user1.follow(user2)
+  before(:all) do
+    @user1 = User.create!(email: 'user1@example.com',
+                          password: 'testtest',
+                          username: 'user1',
+                          visible_name: 'user1')
+    @user2 = User.create!(email: 'user2@example.com',
+                          password: 'testtest',
+                          username: 'user2',
+                          visible_name: 'user2')
+  end
 
-    expect(user1.following).to eq([user2])
+  after(:all) do
+    @user1.destroy
+    @user2.destroy
+  end
+
+  it 'follow other user' do
+    @user1.follow(@user2)
+
+    expect(@user1.following).to eq([@user2])
+    expect(@user2.followers).to eq([@user1])
   end
 end
