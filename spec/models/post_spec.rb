343713ac38c7d36206535b6fab0fba278814105b
Adam Gołąb <edek-150@o2.pl>
2016-06-09 20:51:25 +0200

    posts tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Post, type: :model do
                          visible_name: Faker::Name.name)
     @post1 = Post.create!(content: Faker::Lorem.sentence,
                           user: @user)
+    @post2 = Post.create!(content: Faker::Lorem.sentence,
+                          user: @user,
+                          reply_to: @post1)
   end
   after(:all) do
     @user.destroy
@@ -18,7 +21,22 @@ RSpec.describe Post, type: :model do
     expect(@post1.user).to eq(@user)
   end
 
-  it 'user should have own post' do
-    expect(@user.posts).to eq([@post1])
+  it 'user should have own posts' do
+    expect(@user.posts).to include(@post1, @post2)
+  end
+
+  it 'should have comment' do
+    expect(@post1.replies).to eq([@post2])
+  end
+
+  it 'should be reply to other post' do
+    expect(@post2.reply_to).to eq(@post1)
+  end
+
+  it 'post should not have empty content' do
+    expect do
+      Post.create!(content: '',
+                   user: @user)
+    end.to raise_error(Mongoid::Errors::Validations)
   end
 end
