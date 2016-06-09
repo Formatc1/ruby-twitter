2427ca377d6bdcd31b6f2cbd6fbfe33de6caddd6
Adam Gołąb <edek-150@o2.pl>
2016-06-09 18:56:28 +0200

    create posts tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before(:all) do
-    @user = User.create!(email: 'user1@example.com',
-                         password: 'testtest',
-                         username: 'user1',
-                         visible_name: 'user1')
+    @user = User.create!(email: Faker::Internet.safe_email,
+                         password: Faker::Internet.password(7, 10),
+                         username: Faker::Internet.user_name,
+                         visible_name: Faker::Name.name)
     @post1 = Post.create!(content: Faker::Lorem.sentence,
-                          user: @user1)
+                          user: @user)
   end
   after(:all) do
     @user.destroy
     @post1.destroy
   end
 
+  it 'should create post with author' do
+    expect(@post1.user).to eq(@user)
+  end
 
+  it 'user should have own post' do
+    expect(@user.posts).to eq([@post1])
+  end
 end
